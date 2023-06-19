a21ac7b3cd27dbdfff910356223d8fbffcf3584f
kirill <kicharr@yandex.ru>
2023-06-19 22:50:22 +0300

    add favicon

--- js/main.js
+++ js/main.js
@@ -11,5 +11,3 @@ window.addEventListener('scroll', () => {
     Math.round(pageYOffset) >= 1000 ? document.getElementById('header').classList.add('header-fixed') : document.getElementById('header').classList.remove('header-fixed');
     console.log(windowHeight);
 });
-
-
