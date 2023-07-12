5f2fcf29435fbf4dc0d28a806e1e86e3e82c24c6
kicharr <99511093+kicharr@users.noreply.github.com>
2023-07-13 01:51:58 +0300

    testing pagination

--- js/aboutPage.js
+++ js/aboutPage.js
@@ -1,14 +1,13 @@
 let currentUrl = window.location.href.split('/');
 let aboutPageId = document.getElementById('aboutPageId');
-currentUrl[3] === 'Services.html' ? aboutPageId.classList.add('header-nav__active') : aboutPageId
-    .classList.add('header-nav__active');
+currentUrl[3] === 'Services.html' ? aboutPageId.classList.add('header-nav__active') : aboutPageId.classList.add('header-nav__active');
 
-// const teamWrapper = document.getElementById('teamWrapper');
-// console.log(teamWrapper);
 
 // employees
 let employeesUrl = '../userData/employees.json';
-let responce = fetch(employeesUrl).then(data => data.json()).then(res => setResponce(res));
+let responce = fetch(employeesUrl)
+    .then(data => data.json())
+    .then(res => setResponce(res));
 
 const employeesArray = [];
 
@@ -22,16 +21,20 @@ function setResponce(res) {
     let employeePhoto = document.querySelector('.worker-image');
     let employeePosition = document.querySelector('.worker-position');
 
-
-
     employeesArray.forEach(element => {
         for (let i = 0; i < element.length; i++) {
             let employeeCardClone = employeeCard.cloneNode(true);
             teamWrapper.append(employeeCardClone);
 
+
+            // teamWrapper.className.split(' ')[1] === 'employees-hideen__wrapper' ? employeeCardClone.length = 6 : employeeCardClone.length = 20
+            
             employeePhoto.setAttribute('src', element[i].photo);
             employeeName.innerHTML = element[i].name;
             employeePosition.innerHTML = element[i].position;
+
+            // let countElements = Object.keys(employeeCardClone).length;
+            // console.log(countElements);
         }
     });
 }
@@ -40,15 +43,8 @@ document.getElementById('loaderWrapper').addEventListener('click', () => {
     document.getElementById('loaderCircle').classList.add('loader_active')
 
     setTimeout(() => {
-        if (teamWrapper.className.split(' ')[1] === 'employees-hideen__wrapper') {
-            teamWrapper.classList.remove('employees-hideen__wrapper');
-
-            // console.log('yes');
-        } else {
-            // console.log('no');
-        }
+        (teamWrapper.className.split(' ')[1] === 'employees-hideen__wrapper') ? teamWrapper.classList.remove('employees-hideen__wrapper') : null;
     }, 1000)
-
 
 })
 
